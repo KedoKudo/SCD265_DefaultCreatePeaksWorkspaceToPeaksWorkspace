262a65ae67f7ac58d0a4a2a28dd8b3e376d6cd17
Zhang, Chen <zhangc@ornl.gov>
2021-03-26 09:32:48 -0400

    add testing script

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#-- default, regular PeaksWorkspaces with an instrument specified
+sampleWs = CreateSampleWorkspace()
+pws_withInst = CreatePeaksWorkspace(
+    InstrumentWorkspace=sampleWs,
+    NumberOfPeaks=3,
+    )
+
+#-- defualt, regular Peaksworkspace without specifying an instrument
+pws_withoutInst = CreatePeaksWorkspace(NumberOfPeaks=3)
+
+#-- lean pws, Lean Elastic Peaksworkspace without specifying an instrument
+lpws = CreatePeaksWorkspace(
+    NumberOfPeaks=3,
+    OutputType="Lean",
+    )
+
+#-- lean pws, Lean Elastic Peakworkspace with specifying an instrument, which is ignored
+lpws_ignoreInst = CreatePeaksWorkspace(
+    InstrumentWorkspace=sampleWs,
+    NumberOfPeaks=3,
+    OutputType="Lean",
+    )

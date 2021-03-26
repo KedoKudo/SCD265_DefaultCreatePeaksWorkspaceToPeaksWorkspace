4ed4a44bc6df84525febe9baa4040d19e96e9206
Zhang, Chen <zhangc@ornl.gov>
2021-03-26 12:03:48 -0400

    update testing script

--- test.py
+++ test.py
@@ -14,12 +14,12 @@ pws_withoutInst = CreatePeaksWorkspace(NumberOfPeaks=3)
 #-- lean pws, Lean Elastic Peaksworkspace without specifying an instrument
 lpws = CreatePeaksWorkspace(
     NumberOfPeaks=3,
-    OutputType="Lean",
+    OutputType="LeanElasticPeak",
     )
 
 #-- lean pws, Lean Elastic Peakworkspace with specifying an instrument, which is ignored
 lpws_ignoreInst = CreatePeaksWorkspace(
     InstrumentWorkspace=sampleWs,
     NumberOfPeaks=3,
-    OutputType="Lean",
+    OutputType="LeanElasticPeak",
     )
